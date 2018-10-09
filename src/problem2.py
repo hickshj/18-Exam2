ec6ac6ecda221e601319024031c4f320543c0863
hickshj <hickshj@rose-hulman.edu>
2018-10-09 18:48:52 -0400

    In the Hall of the mountain king

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 2, problem 2.
 
 Authors: Dave Fisher, David Mutchler, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Hunter Hicks.  October 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import math
 import time
