bab76adc6b7e2afa3be91598adf3a18ecfc012d4
hickshj <hickshj@rose-hulman.edu>
2018-10-09 19:02:44 -0400

    In the Hall of the mountain king

--- src/problem3.py
+++ src/problem3.py
@@ -25,7 +25,7 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_init()
+    run_test_init()
     # run_test_rain()
     # run_test_get_total_rain_amount()
     # run_test_merge_cloud()
@@ -76,8 +76,13 @@ class Cloud(object):
           :type water: int | float
         """
         # ---------------------------------------------------------------------
-        # TODO: 2. Implement and test this method.
+        # Done: 2. Implement and test this method.
         # ---------------------------------------------------------------------
+        self.capacity = capacity
+        if water > self.capacity:
+            self.water = capacity
+        else:
+            self.water = water
 
     def rain(self, rain_amount):
         """
