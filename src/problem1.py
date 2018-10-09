6d38f3f2db3f805c7367b304bd771e56c0ff4672
hickshj <hickshj@rose-hulman.edu>
2018-10-09 18:35:08 -0400

    In the Hall of the mountain king

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Hunter Hicks.  October 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -49,7 +49,7 @@ def main():
     print(' to run the testing code as you complete the TODOs.')
 
     # run_test_problem1a()
-    # run_test_problem1b()
+    run_test_problem1b()
     # run_test_problem1c()
     # run_test_problem1d()
 
@@ -218,9 +218,15 @@ def problem1a(strings):
       :rtype: list of int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DOne: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    new_list = []
+    for k in range(len(strings)):
+        q = strings[k]
+        length = len(q)
+        new_list = new_list + [length]
+    return new_list
 
 
 def run_test_problem1b():
